6a33de277f6b3857ff040077583670425ffd2c59
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-25 14:43:34 +0100

    server up and running in the backend

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,28 @@
 import express from "express";
 import {config} from "dotenv"
 import cors from "cors"
+import cookieParser from "cookie-parser"
+import fileUploader from "express-fileupload"
 
 const app = express();
-config({path: "./config/config.env"});
+config({path: ".env"});
 
-app.use(cors({
-    origin: [
-        process.env.FRONTEND_URL,
-        process.env.DASHBOARD_URL
-    ]
-}));
+
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+        methods: ["GET", "POST", "PUT", "UPDATE", "DELETE"],
+        credentials: true,
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.use(fileUploader({
+    useTempFiles: true,
+    tempFileDir: "/tmp"
+}))
 
 export default app;
