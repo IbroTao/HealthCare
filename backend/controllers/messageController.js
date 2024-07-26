61d9e6883d46ffa2603a12a04fa98cda1109e0c7
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-26 11:33:51 +0100

    controller set up for user

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,9 +1,16 @@
+import { Message } from "../models/messageModel";
+
 export const sendMessage = async(req, res, next) => {
     const {firstName, lastName, email, phone, message} = req.body;
     if(!firstName || !lastName || !email || !phone || !message) {
         return res.status(400).json({
             success: false,
             message: "Please Fill Full Form"
-        })
+        });
     }
+        await Message.create({firstName, lastName, email, phone, message});
+        res.status(200).json({
+            success: true,
+            message: "Message Send Successful"
+        })
 }
