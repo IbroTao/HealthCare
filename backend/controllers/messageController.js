1046459220322a87fac2723ae1ff7b08b89775b5
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-26 12:36:31 +0100

    user routes configured in the index file

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,16 +1,21 @@
 import { Message } from "../models/messageModel.js";
 
 export const sendMessage = async(req, res, next) => {
-    const {firstName, lastName, email, phone, message} = req.body;
-    if(!firstName || !lastName || !email || !phone || !message) {
-        return res.status(400).json({
-            success: false,
-            message: "Please Fill Full Form"
-        });
-    }
+    try{
+        const {firstName, lastName, email, phone, message} = req.body;
+        if(!firstName || !lastName || !email || !phone || !message) {
+            return res.status(400).json({
+                success: false,
+                message: "Please Fill Full Form"
+            });
+        }
         await Message.create({firstName, lastName, email, phone, message});
         res.status(200).json({
             success: true,
             message: "Message Send Successful"
         })
+    }
+    catch(err){
+        res.status(500).json({err})
+    }
 }
--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -2,33 +2,32 @@ import mongoose from "mongoose";
 import validator from "validator";
 
 const messageSchema = new mongoose.Schema({
-    firstName: {
+    firstName:{
         type: String,
         required: true,
-        minlength: [3, "First Name Must Contain At Least 3 Characters!"]
+        minLength: [3, "First Name Must Contain At Least 3 Characters!"],
     },
-    lastName: {
+    lastName:{
         type: String,
         required: true,
-        minlength: [3, "Last Name Must Contain At Least 3 Characters!"]
+        minLength: [3, "Last Name Must Contain At Least 3 Characters!"],
     },
-    email: {
+    email:{
         type: String,
         required: true,
         validate: [validator.isEmail, "Please Provide A Valid Email!"],
         unique: true
     },
-    phone: {
+    phone:{
         type: Number,
         required: true,
-        minlength: [11, "Phone Number Must Contain 11 Digits!"],
-        maxlength: [11, "Phone Number Must Contain Exactly 11 Digits!"],
-        unique: true
+        minLength: [11, "Phone Number Must Contain 11 Digits!"],
+        maxLength: [11, "Phone Number Must Contain Exactly 11 Digits!"],
     },
-    message: {
+    message:{
         type: String,
         required: true,
-        minlength: [10, "Message Must Contain At Least 10 Characters!"]
+        minLength: [10, "Message Must Contain At Least 10 Characters!"],
     }
 });
 
