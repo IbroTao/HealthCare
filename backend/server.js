23af4a1bc7b32a6eb6172b58eb455ba16a3bebc1
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-26 11:01:21 +0100

    cloundinary credentials set for upload of images

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 import app from "./app.js"
 import dotenv from "dotenv";
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_NAME
+})
 
 dotenv.config();
 
