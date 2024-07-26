5339d26babe24458ecabb5c6119cfe87b7be18a6
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-26 11:19:18 +0100

    model set up for users

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -11,5 +11,19 @@ const messageSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: [3, "Last Name Must Contain At Least 3 Characters!"]
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: [validator.isEmail, "Please Provide A Valid Email!"]
+    },
+    phone: {
+        type: Number,
+        required: true,
+        minlength: [11, "Phone Number Must Contain 11 Digits!"],
+        maxlength: [11, "Phone Number Must Contain 11 Digits!"]
+    },
+    message: {
+        type: String,
     }
 }) 
