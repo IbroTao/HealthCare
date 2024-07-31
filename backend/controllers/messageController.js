e417e34eac25fa3107b2791466f83fe87f2e89ad
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-31 12:43:39 +0100

    middlewares set up for error handling and async functions

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,14 +1,12 @@
 import { Message } from "../models/messageModel.js";
 import {catcAsyncErrors} from "../middlewares/catchAsyncErrors.js"
+import {ErrorHandler} from "../middlewares/errorMiddlewares.js"
 
 export const sendMessage = catcAsyncErrors(async(req, res, next) => {
     try{
         const {firstName, lastName, email, phone, message} = req.body;
         if(!firstName || !lastName || !email || !phone || !message) {
-            return res.status(400).json({
-                success: false,
-                message: "Please Fill Full Form"
-            });
+            return next(new ErrorHandler("Please Fill Full Form!", 400))
         }
         await Message.create({firstName, lastName, email, phone, message});
         res.status(200).json({
--- backend/middlewares/errorMiddlewares.js
+++ backend/middlewares/errorMiddlewares.js
@@ -30,3 +30,5 @@ export const errorMiddleware = (err, req, res, next) => {
         message: err.message
     })
 }
+
+export default ErrorHandler;
