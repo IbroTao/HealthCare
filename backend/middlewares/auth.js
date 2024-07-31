c5c8e413c4f2f7659c0011e22ced54fef9631b8f
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-31 22:36:53 +0100

    middleware to authenticate admin

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,3 +1,9 @@
 import {catcAsyncErrors} from "./catchAsyncErrors.js"
+import ErrorHandler from "./errorMiddlewares.js"
 
-export const isAdminAuthenticated 
+export const isAdminAuthenticated = catcAsyncErrors(async(err, req, res, next) => {
+    const token = req.cookies.adminToken;
+    if(!token) {
+        return next(new ErrorHandler)
+    }
+})
