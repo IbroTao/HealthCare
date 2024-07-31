c114073f23aa05f1e338f84e541bc443a610b5fb
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-31 13:20:27 +0100

    password hashing and comparsion set up in models

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -67,4 +67,10 @@ userSchema.pre("save", async function(next) {
     this.password = await bcrypt.hash(this.password, 10)
 })
 
+userSchema.methods.comparePassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.methods.generate
+
 export const User = mongoose.model("User", userSchema)
