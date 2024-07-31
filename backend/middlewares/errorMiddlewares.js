99576b7eea5171556ccb59b406291f27236b24ba
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-31 12:32:11 +0100

    middlewares set up for error handling

--- backend/middlewares/errorMiddlewares.js
+++ backend/middlewares/errorMiddlewares.js
@@ -11,6 +11,9 @@ export const errorMiddleware = (err, req, res, next) => {
 
     if(err.code === 11000) {
         const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
-        err = new ErrorHandler()
+        err = new ErrorHandler(message, 400)
+    }
+    if(err.name === "JsonWebTokenError") {
+        const message = "Json Web Token is invalid, Try Again!"
     }
 }
