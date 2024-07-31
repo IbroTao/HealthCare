52caa433ee92e553dd51834915c66959149f75b0
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-31 22:39:51 +0100

    middleware to authenticate admin

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,14 @@
-import {catcAsyncErrors} from "./catchAsyncErrors.js"
-import ErrorHandler from "./errorMiddlewares.js"
+
+import {catcAsyncErrors} from "./catchAsyncErrors.js";
+import ErrorHandler from "./errorMiddlewares.js";
+import jwt from "jsonwebtoken"
 
 export const isAdminAuthenticated = catcAsyncErrors(async(err, req, res, next) => {
     const token = req.cookies.adminToken;
     if(!token) {
-        return next(new ErrorHandler)
+        return next(new ErrorHandler("Admin Not Authenticated!", 400))
     }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User
 })
