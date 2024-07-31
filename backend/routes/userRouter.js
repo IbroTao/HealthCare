ae2cc57386be514e380617e980ca59a083aa54b8
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-31 18:49:18 +0100

    admin routes configured and set up

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { loginPatient, registerPatient } from "../controllers/userController.js";
+import { addAdmin, loginPatient, registerPatient } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post('/patient/register', registerPatient);
-router.post('/patient/login', loginPatient)
+router.post('/patient/login', loginPatient);
+router.post('/admin/new', addAdmin);
 
 export default router;
