7c36c75a0deb36247e2fb1ffbdbc372ea8ba17a3
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-31 23:28:08 +0100

    middleware to authenticate patient set up

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addAdmin, loginPatient, registerPatient } from "../controllers/userController.js";
+import {isAdminAuthenticated, isPatientAuthenticated} from "../middlewares/auth.js"
 
 const router = express.Router();
 
 router.post('/patient/register', registerPatient);
 router.post('/login', loginPatient);
-router.post('/admin/new', addAdmin);
+router.post('/admin/new', isAdminAuthenticated, addAdmin);
 
 export default router;
