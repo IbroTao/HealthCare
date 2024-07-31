c2265bf91115888c82e9cbf207f61a028532babf
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-31 17:30:13 +0100

    login route added

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { registerPatient } from "../controllers/userController.js";
+import { loginPatient, registerPatient } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post('/patient/register', registerPatient)
+router.post('/patient/register', registerPatient);
+router.post('/patient/login', loginPatient)
 
 export default router;
