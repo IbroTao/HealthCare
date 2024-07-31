fcb3751bcd9286ee8812394186040530dec4cf16
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-31 23:21:54 +0100

    middleware to authenticate patient set up

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,7 @@ import {catcAsyncErrors} from "./catchAsyncErrors.js";
 import ErrorHandler from "./errorMiddlewares.js";
 import jwt from "jsonwebtoken"
 
-export const isAdminAuthenticated = catcAsyncErrors(async(err, req, res, next) => {
+export const isAdminAuthenticated = catcAsyncErrors(async(req, res, next) => {
     const token = req.cookies.adminToken;
     if(!token) {
         return next(new ErrorHandler("Admin Not Authenticated!", 400))
@@ -15,4 +15,20 @@ export const isAdminAuthenticated = catcAsyncErrors(async(err, req, res, next) =
     if(req.user.role !== "Admin") {
         return next(new ErrorHandler(`${req.user.role} not authorized for this resources!`))
     }
+    next();
+})
+
+export const isPatientAuthenticated = catcAsyncErrors(async(req, res, next) => {
+    const token = req.cookies.patientToken;
+    if(!token) {
+        return next(new ErrorHandler("Patient Not Authenticated!", 400))
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User.findById(decodedToken.id);
+    
+    if(req.user.role !== "Patient") {
+        return next(new ErrorHandler(`${req.user.role} not authorized for this resources!`))
+    }
+    next();
 })
