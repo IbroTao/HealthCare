9ba8e9696b679b8194b1f37f2f39d8969260511d
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-01 09:38:39 +0100

    retrieval of doctors set up

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { addAdmin, loginPatient, registerPatient } from "../controllers/userController.js";
+import { addAdmin, getAllDoctors, loginPatient, registerPatient } from "../controllers/userController.js";
 import {isAdminAuthenticated, isPatientAuthenticated} from "../middlewares/auth.js"
 
 const router = express.Router();
 
+// POST ROUTES
 router.post('/patient/register', registerPatient);
 router.post('/login', loginPatient);
 router.post('/admin/new', isAdminAuthenticated, addAdmin);
+
+// GET ROUTES
+router.get('/doctors', getAllDoctors);
 
 export default router;
