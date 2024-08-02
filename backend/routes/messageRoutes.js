4344ba465bb2a7e6edc82fcb338968d4b13c11ca
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-02 10:39:55 +0100

    avatar for doctor set up

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { sendMessage } from "../controllers/messageController.js";
+import { getAllMessages, sendMessage } from "../controllers/messageController.js";
+import {isAdminAuthenticated} from "../middlewares/auth.js"
 
 const router = express.Router();
 
 router.post('/send', sendMessage);
+router.post('get-all-msgs', isAdminAuthenticated, getAllMessages)
 
 export default router;
