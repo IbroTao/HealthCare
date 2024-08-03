aa2fbff244335c3e538e7add32fd6e06ec00b39f
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-03 16:06:22 +0100

    controller set up for appointment btw patients & doctors

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,2 +1,4 @@
 import {catchAsyncErrors} from "../middlewares/catchAsyncErrors";
-import ErrorHandler from "../middlewares/errorMiddlewares"
+import ErrorHandler from "../middlewares/errorMiddlewares";
+import {User} from "../models/userModel.js";
+import {Appointment} from "../models/appointmentSchema.js"
