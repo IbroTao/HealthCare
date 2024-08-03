19f84f4f151bca584f42a80eb595db84219b287e
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-03 16:36:26 +0100

    controller set up for appointment btw patients & doctors

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,7 +1,7 @@
 import {catchAsyncErrors} from "../middlewares/catchAsyncErrors";
 import ErrorHandler from "../middlewares/errorMiddlewares";
 import {User} from "../models/userModel.js";
-import {Appointment} from "../models/appointmentSchema.js"
+import {Appointment} from "../models/appointmentModel.js"
 
 export const postAppointment = catchAsyncErrors(async(req, res) => {
     const {firstName, lastName, email, phone, dob, 
@@ -9,9 +9,19 @@ export const postAppointment = catchAsyncErrors(async(req, res) => {
         department, doctor_firstName, doctor_lastName} 
     = req.body;
 
-    if(firstName, lastName, email, phone, dob, 
-        nic, hasVisited, gender, appointment_date, 
-        department, doctor_firstName, doctor_lastName) {
-            
-        }
+    if(!firstName 
+        || !lastName 
+        || !email 
+        || !phone 
+        || !dob 
+        || !nic 
+        || !hasVisited 
+        || !gender 
+        || !appointment_date 
+        || !department 
+        || !doctor_firstName 
+        || !doctor_lastName
+    ) {
+        return next(new ErrorHandler("Please Fill Full Form", 400))
+    }
 })
