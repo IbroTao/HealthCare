bb2f39dc9905454286cc77a2e332b8467420b6f4
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-03 17:38:08 +0100

    appointment routes configured in index file

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
+import { bookAppointment } from "../controllers/appointmentController";
+import {isPatientAuthenticated} from "../middlewares/auth.js"
 
 const router = express.Router();
 
+router.post('/book', isPatientAuthenticated, bookAppointment)
 
 export default router;
