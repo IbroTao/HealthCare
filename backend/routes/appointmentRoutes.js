5befde67f47eb61dffe1bb968e662720e0928612
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-03 19:03:08 +0100

    get all appointents made set up

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { bookAppointment } from "../controllers/appointmentController.js";
-import {isPatientAuthenticated} from "../middlewares/auth.js"
+import { bookAppointment, getAllAppointments } from "../controllers/appointmentController.js";
+import {isAdminAuthenticated, isPatientAuthenticated} from "../middlewares/auth.js"
 
 const router = express.Router();
 
-router.post('/book', isPatientAuthenticated, bookAppointment)
+router.post('/book', isPatientAuthenticated, bookAppointment);
+router.get('/all', isAdminAuthenticated, getAllAppointments);
 
 export default router;
