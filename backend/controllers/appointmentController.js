6a36d421d486d502ceddf67436aadd69f9ffff14
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-03 17:00:51 +0100

    controller set up for appointment btw patients & doctors

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -24,4 +24,40 @@ export const postAppointment = catchAsyncErrors(async(req, res) => {
     ) {
         return next(new ErrorHandler("Please Fill Full Form", 400))
     }
-})
+
+    const findScheduledAppt = await User.find({
+        firstName: doctor_firstName,
+        lastName: doctor_lastName,
+        role: "Doctor",
+        doctorDepartment: department
+    });
+
+    if(findScheduledAppt.length === 0) {
+        return next(new ErrorHandler("Doctor Not Found!", 404))
+    }
+    if(findScheduledAppt > 1) {
+        return next(new ErrorHandler("Doctors Conflict! Please Contact Hospital Through Email Or Phone!", 404))
+    }
+
+    const doctorId = findScheduledAppt[0]._id;
+    const patientId = req.user._id;
+
+    const appointment = await Appointment.create({
+        firstName, 
+        lastName, 
+        email, 
+        phone, 
+        dob, 
+        nic, 
+        hasVisited, 
+        gender, 
+        appointment_date, 
+        department, 
+        doctor: {
+            firstName: doctor_firstName, 
+            lastName: doctor_lastName,
+        },
+        doctorId: doctorId,
+        patientId: patientId
+    })
+}) 
