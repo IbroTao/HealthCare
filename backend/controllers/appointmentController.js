6b6c1f9165bab1550491f360d46ea1c559e50654
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-04 17:34:41 +0100

    appointment updating set up

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -3,26 +3,13 @@ import ErrorHandler from "../middlewares/errorMiddlewares.js";
 import {User} from "../models/userModel.js";
 import {Appointment} from "../models/appointmentModel.js"
 
-export const bookAppointment = catchAsyncErrors(async(req, res) => {
-    const {firstName, lastName, email, phone, dob, 
-        nic, hasVisited, gender, appointment_date, 
-        department, doctor_firstName, doctor_lastName} 
-    = req.body;
+export const bookAppointment = catchAsyncErrors(async (req, res, next) => {
+    const { firstName, lastName, email, phone, dob, nic, gender, appointment_date, 
+        department, doctor_firstName, doctor_lastName } = req.body;
 
-    if(!firstName 
-        || !lastName 
-        || !email 
-        || !phone 
-        || !dob 
-        || !nic 
-        || !hasVisited 
-        || !gender 
-        || !appointment_date 
-        || !department 
-        || !doctor_firstName 
-        || !doctor_lastName
-    ) {
-        return next(new ErrorHandler("Please Fill Full Form", 400))
+    if (!firstName || !lastName || !email || !phone || !dob || !nic 
+        || !gender || !appointment_date || !department || !doctor_firstName || !doctor_lastName) {
+        return next(new ErrorHandler("Please Fill Full Form", 400));
     }
 
     const findScheduledAppt = await User.find({
@@ -32,40 +19,43 @@ export const bookAppointment = catchAsyncErrors(async(req, res) => {
         doctorDepartment: department
     });
 
-    if(findScheduledAppt.length === 0) {
-        return next(new ErrorHandler("Doctor Not Found!", 404))
+    if (findScheduledAppt.length === 0) {
+        return next(new ErrorHandler("Doctor Not Found!", 404));
     }
-    if(findScheduledAppt > 1) {
-        return next(new ErrorHandler("Doctors Conflict! Please Contact Hospital Through Email Or Phone!", 404))
+
+    if (findScheduledAppt.length > 1) {
+        return next(new ErrorHandler("Doctors Conflict! Please Contact Hospital Through Email Or Phone!", 404));
     }
 
     const doctorId = findScheduledAppt[0]._id;
     const patientId = req.user._id;
 
     const appointment = await Appointment.create({
-        firstName, 
-        lastName, 
-        email, 
-        phone, 
-        dob, 
-        nic, 
-        hasVisited, 
-        gender, 
-        appointment_date, 
-        department, 
+        firstName,
+        lastName,
+        email,
+        phone,
+        dob,
+        nic,
+        hasVisited,
+        gender,
+        appointment_date,
+        department,
         doctor: {
-            firstName: doctor_firstName, 
+            firstName: doctor_firstName,
             lastName: doctor_lastName,
         },
-        doctorId, 
-        patientId
+        doctorId,
+        patientId,
+        hasVisited: false
     });
 
     res.status(201).json({
         success: "true",
-        message: "Appointment Boooked Successfully!"
-    })
-}) 
+        message: "Appointment Booked Successfully!"
+    });
+});
+
 
 export const getAllAppointments = catchAsyncErrors(async(req, res) => {
     const appointments = await Appointment.find();
