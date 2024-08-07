a6d3e8abd761e1c6dcb9925b1c806e2d22cba51a
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-07 10:14:09 +0100

    toast set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Appointment from "./pages/Appointment";
+import AboutUs from "./pages/AboutUs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home"
+import {ToastContainer, toast} from "react-toastify";
+import "react-toastify/dist/"
 
 const App = () => {
   return (
@@ -8,8 +15,13 @@ const App = () => {
       <>
         <Router>
           <Routes>
-            <Route/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/appointment" element={<Appointment/>}/>
+            <Route path="/about" element={<AboutUs/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
           </Routes>
+          <ToastContainer position="top-center"/>
         </Router>
       </>
     </div>
