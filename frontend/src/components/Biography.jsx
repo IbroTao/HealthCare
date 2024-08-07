f2c4b3eee994e7ed57b408122c736ab035ee3d73
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-07 14:19:47 +0100

    home page working with bckg images

--- frontend/src/components/Biography.jsx
+++ frontend/src/components/Biography.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
-const Biography = () => {
+const Biography = ({imageUrl}) => {
   return (
-    <div>
-      
+    <div className='container biography'>
+      <div className="banner">
+        <img src={imageUrl} alt="aboutImg" />
+      </div>
+      <div className="banner">
+        <p>Biography</p>
+        <h3>Who We Are</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium sint incidunt reiciendis assumenda dolorum autem, 
+          minima ut harum rem non ipsum praesentium fugiat, veniam deleniti quasi quibusdam inventore tempora voluptatum suscipit? 
+          Numquam placeat quia, veritatis et, ullam vel consequatur iste repellat mollitia nostrum nihil eveniet ducimus molestias quas, suscipit repellendus!
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> 
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odit ratione voluptatum nam labore magnam iusto excepturi iste, 
+          neque quidem! Odit ratione eligendi veniam ducimus! Ab nostrum quas quisquam unde non sed dignissimos corporis dolores?
+        </p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum, perferendis est. Officiis!</p>
+        <p>Lorem, ipsum dolor.</p>
+      </div>
     </div>
   )
 }
