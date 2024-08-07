50e3bed566521ff0e45d06913f51c13a11e647cd
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-07 15:46:57 +0100

    home page working with bckg images

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -21,6 +21,10 @@ const MessageForm = () => {
           <input type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
           <input type="number" placeholder='Phone Number' value={phone} onChange={(e) => setPhone(e.target.value)}/>
         </div>
+        <textarea rows={7} placeholder='Message' value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+        <div style={{justifyContent: "center", alignItems: "center" }}>
+          <button type="submit">Send</button>
+        </div>
       </form>     
     </div>
   )
