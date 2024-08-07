e77a64e90a6ecae9b85aa9108f4fe69bc1cdaf0c
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-07 14:59:39 +0100

    home page working with bckg images

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MessageForm = () => {
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [message, setMessage] = useState("")
+  const handleMessage = (e) => {
+    e.preventDefault();
+  }
   return (
-    <div>
-      
+    <div className='container form-component message-form'>
+      <h2>Send Us A Message</h2> 
+      <form onSubmit={handleMessage}>
+        <div>
+          <input type="text" placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+          <input type="text" placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+        </div>
+        <div>
+          <input type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input type="number" placeholder='Phone Number' value={phone} onChange={(e) => setPhone(e.target.value)}/>
+        </div>
+      </form>     
     </div>
   )
 }
