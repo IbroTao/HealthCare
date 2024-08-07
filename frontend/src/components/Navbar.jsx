a989dcfaf731b1c9c93762dbbe6330af71221166
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-07 22:23:46 +0100

    navbar & logo for home page specified and set up

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 
 const Navbar = () => {
-    const [show, setShow] = useState()
+    const [show, setShow] = useState(false)
   return (
     <div className='container'>
         <div className="logo">DivexCare</div>
-        <div className={show ? "navLinks showmenu": "navLinks"}></div>
+        <div className={show ? "navLinks showmenu": "navLinks"}>
+            <div className="links">
+                <Link to={'/'}>HOME</Link>
+                <Link to={'/appointment'}>APPOINTMENT</Link>
+                <Link to={'/about'}>ABOUT US</US></Link>
+            </div>
+        </div>
     </div>
   )
 }
