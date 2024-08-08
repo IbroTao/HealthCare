5d4c4aa8805cd03a613e12cb75ef713d007e0f37
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-08 17:18:04 +0100

    login auth & verification set up in frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,48 +1,97 @@
-import React, { useContext, useState } from 'react'
-import { Context } from '../main'
-import { Navigate, useNavigate, Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { Context } from "../main";
+import { Link, useNavigate, Navigate } from "react-router-dom";
 
 const Login = () => {
-    const {isAuthenticated, setIsAuthenticated} = useContext(Context);
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    const navigateTo = useNavigate();
+  const navigateTo = useNavigate();
 
-    const handleLogin = async(e) => {
-        e.preventDefault();
-        try{
-            const response = await axios.post("http://localhost:4000/api/v1/user/login", {email, password, confirmPassword, role: "Patient"}, {
-                withCredentials: true,
-                headers: {"Content-Type": "application/json"}
-            })
-        }
-    };
-
-    if(isAuthenticated){
-        return <Navigate to={'/'}/>
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await axios
+        .post(
+          "http://localhost:4000/api/v1/user/login",
+          { email, password, confirmPassword, role: "Patient" },
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+        .then((res) => {
+          toast.success(res.data.message);
+          setIsAuthenticated(true);
+          navigateTo("/");
+          setEmail("");
+          setPassword("");
+          setConfirmPassword("");
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
+  };
 
-    return (
-        <div className='container form-component login-form'>
-            <h2>Sign In</h2>
-            <p>Please Login To Continue</p>
-            <form onSubmit={handleLogin}>
-                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email'/>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password'/>
-                <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder='Confirm Password'/>   
-                <div style={{gap: "10px", justifyContent: "flex-end", flexDirection: "row"}}>
-                    <p style={{marginBottom: 0}}>Not Registered?</p>
-                    <Link to={'/register'} style={{textDecoration: "none", alignItems: "center"}}>Register Now</Link>
-                </div>
-                <div style={{justifyContent: "center", alignItems: "center"}}>
-                    <button type="submit">Login</button>
-                </div>
-            </form>
-        </div>
-    )
-}
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
 
-export default Login
+  return (
+    <>
+      <div className="container form-component login-form">
+        <h2>Sign In</h2>
+        <p>Please Login To Continue</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat culpa
+          voluptas expedita itaque ex, totam ad quod error?
+        </p>
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <div
+            style={{
+              gap: "10px",
+              justifyContent: "flex-end",
+              flexDirection: "row",
+            }}
+          >
+            <p style={{ marginBottom: 0 }}>Not Registered?</p>
+            <Link
+              to={"/register"}
+              style={{ textDecoration: "none", color: "#271776ca" }}
+            >
+              Register Now
+            </Link>
+          </div>
+          <div style={{ justifyContent: "center", alignItems: "center" }}>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import {Context} from '../main'
 
 const Register = () => {
+  const {isAuthenticated, setIsAuthenticated} = useContext(Context)
   return (
     <div>Register</div>
   )
