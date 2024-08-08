9f1341c8ac49af1d60c90df6e3c1479989b7fbd6
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-08 16:37:48 +0100

    page for user & medical login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { Context } from '../main'
-import { Navigate, useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate, Link } from 'react-router-dom';
 
 const Login = () => {
     const {isAuthenticated, setIsAuthenticated} = useContext(Context);
@@ -27,8 +27,15 @@ const Login = () => {
             <form onSubmit={handleLogin}>
                 <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email'/>
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password'/>
-                <input type="password" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email'/>   
+                <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder='Confirm Password'/>   
                 <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email'/>
+                <div style={{gap: "10px", justifyContent: "flex-end", flexDirection: "row"}}>
+                    <p style={{marginBottom: 0}}>Not Registered?</p>
+                    <Link to={'/register'} style={{textDecoration: "none", alignItems: "center"}}>Register Now</Link>
+                </div>
+                <div style={{justifyContent: "center", alignItems: "center"}}>
+                    <button type="submit">Login</button>
+                </div>
             </form>
         </div>
     )
