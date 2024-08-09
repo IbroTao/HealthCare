db68f6f7e3c467f23c984d819fa5de340c2b1bcb
Taofeek <ibrahimtaofeek005@gmail.com>
2024-08-09 10:22:52 +0100

    register page set up

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -29,6 +29,15 @@ const Register = () => {
     <h2>Sign Up</h2>
     <p>Please Sign Up To Continue</p>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat labore, adipisci enim temporibus quod sint?</p>
+    <form onSubmit={handleRegister}>
+      <div>
+        <input type="text" placeholder='First Name' value={firstName} onChange={(e)=> setFirstName(e.target.value)}/>
+        <input type="text" placeholder='Last Name' value={lastName} onChange={(e)=> setLastName(e.target.value)}/>
+      </div>
+      <div>
+        <input type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+      </div>
+    </form>
   </div>
 }
 
